9ba72b6f718c9516f521607da8734a51fa11303f
Osama Azab <gosamab@hotmail.com>
2023-12-09 17:42:23 +0300

    add upcoming videos

--- app/Video/page.jsx
+++ app/Video/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import profile from "../../public/images/channel_profile.jpg";
+import Video from "@/app/Components/Video/Video";
 
 function VideoPage() {
   const embedUrl = `https://www.youtube.com/embed/Xh26-fF6fTM?autoplay=1&mute=0`;
@@ -11,29 +12,18 @@ function VideoPage() {
         style={{ height: "65vh" }}
         src={embedUrl}
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-      <div>
-        <div className="w-full flex flex-start group-active:bg-backgroundGray ">
-          <div className=" w-10">
-            <Image src={profile} className=" rounded-full w-8" />
+      <div className="flex w-full mt-4 mb-4 mx-auto space-x-4">
+        <div className="flex flex-col w-3/5 ml-8 border-0 rounded-lg">
+          <div className="w-10">
+            <Image src={profile} className="rounded-full w-8" />
           </div>
-          <div className="w-60">
-            <div className=" w-60  text-white truncate">
-              <span className=" font-textFont font-bold mx-1 w-full">
-                Next.js Projects: Build a Full-stack App with Next.js, Tailwind,
-                Radix UI, and Prisma
-              </span>
-            </div>
-            <div className="w-full text-gray-600 text-sm font-textFont whitespace-nowrap">
-              <span className="mx-1 block ">Programming with Mosh</span>
-              <span className="mx-1 inline-block mt-1"> 91k views </span>
-              <span className="font-bold text-lg">&#183;</span>
-              <span className="mx-1 inline-block">3 months ago </span>
-            </div>
-          </div>
+          <div className="w-full"></div>
+        </div>
+        <div className="flex flex-col w-2/5 border-0 rounded-lg">
+          <Video></Video>
         </div>
       </div>
     </div>
